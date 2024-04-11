c1ad65d77d27d8677947b4e1ed5a3b724898fad7
luankaio <luankaiosilveira45@hotmail.com>
2024-04-11 15:11:32 -0300

    implementei a fisica do carro ser jogado para fora na curva

--- Main.java
+++ Main.java
@@ -6,12 +6,8 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         Segmento segmento = new Segmento();
-        Carro carro = new Carro();
-
         frame.setLocationRelativeTo(null);
         frame.add(segmento);
-        frame.add(carro);
-        frame.setResizable(false);
         frame.setVisible(true);
         Thread t = new Thread(segmento);
         t.start();
--- Segmento.java
+++ Segmento.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyListener;
 
-//import java.awt.event.KeyEvent;
-//import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Segmento extends JPanel implements Runnable {
+public class Segmento extends JPanel implements Runnable, KeyListener {
     private static final long serialVersionUID = 1L;
     private int largura = 400; // Largura do retângulo
     private int altura = 20; // Altura do retângulo
@@ -15,12 +16,14 @@ public class Segmento extends JPanel implements Runnable {
     private int direcao = 0;
     private int m=0;
     private int ç=0;
-    ImageIcon imagemCarro = new ImageIcon("./carro.png");
+    private int curva=0;
+    private String image = "./carro.png";
+    ImageIcon imagemCarro = new ImageIcon(image);
     Percurso percurso = new Percurso();
 
     public Segmento() {
         setBackground(Color.WHITE);
-        //addKeyListener(this); // Adiciona o KeyListener ao painel
+        addKeyListener(this); // Adiciona o KeyListener ao painel
         setFocusable(true); // Permite que o painel tenha o foco para detectar eventos de teclado
     }
 
@@ -32,6 +35,9 @@ public class Segmento extends JPanel implements Runnable {
 
     }
 
+    public void direcionamentoCarro(){
+       
+    }
     
 
     private void desenharRetangulo(Graphics g) {
@@ -51,43 +57,12 @@ public class Segmento extends JPanel implements Runnable {
             this.y -= this.altura;   // defino a altura do quadrado pra sempre ser um acima do outro quadrante
             this.altura-=i/17;
             this.largura -= diminuirTamanho; //diminuo a largura em 30 para cada novo quadrante
-            this.x += diminuirTamanho / 2;
+            this.x += (diminuirTamanho / 2);
             this.x -= (i * this.k) / 100;
             m++;
         }
         m++;
     }
-
-    private void reset() {
-        this.y = 750;
-        this.x = (800-650)/2;
-        this.largura = 650;
-        this.altura = 15;
-    }
-    /*private void sudoReset() {
-        this.y = 750;
-        this.x = 280;
-        this.largura = 300;
-        this.altura = 10;
-        this.k=0;
-    }*/
-    
-
-
-    public void percurso() {
-        
-        if (percurso.getDirecao()==1) {
-            curvaEsquerda();
-        }
-        if (percurso.getDirecao()==2) {
-            curvaDireita();
-        }
-        if (percurso.getDirecao()==0) {
-            reta();
-        }
-    }
-
-    
     
     @Override//runnable que roda o codigo
     public void run() {
@@ -112,6 +87,34 @@ public class Segmento extends JPanel implements Runnable {
         }
     }
     
+    
+    private void reset() {
+        this.y = 750;
+        this.x = ((800-650)/2)+curva;
+        this.largura = 650;
+        this.altura = 15;
+    }
+    
+ 
+    //faz com que a pista curve e com que o carro seja jogado para a esqueda e direita
+    public void percurso() {
+        
+        if (percurso.getDirecao()==1) {
+            curvaEsquerda();
+            this.curva-=1;
+        }
+        if (percurso.getDirecao()==2) {
+            curvaDireita();
+            this.curva+=1;
+        }
+        if (percurso.getDirecao()==0) {
+            reta();
+        }
+    }
+
+    
+    
+    
     public void moverQuadrantes() {
         if (ç != 0) {
             if (k > 0 && k != 0) {
@@ -133,16 +136,8 @@ public class Segmento extends JPanel implements Runnable {
         
     }
 
-
-    // Métodos necessários da interface KeyListener
-    //@Override
-    //public void keyTyped(KeyEvent e) {
-    //}
-    //@Override
-    //public void keyReleased(KeyEvent e) {
-    //}
-
-
+    
+    
     public void curvaEsquerda(){
         this.direcao = 1;
         ç = 0;
@@ -154,5 +149,40 @@ public class Segmento extends JPanel implements Runnable {
     public void reta(){
         ç = 1;
     }
+    
+    public void keyPressed(KeyEvent tecla) {
+        int codigo = tecla.getKeyCode();
+        
+        if (codigo == KeyEvent.VK_LEFT) {
+            this.curva+=20;
+            this.image="./carroesquerda.png";
+            this.imagemCarro = new ImageIcon(image);
+            repaint();
+        }
+        if (codigo == KeyEvent.VK_RIGHT) {
+            this.curva-=20;
+            this.image="./carrodireita.png";
+            this.imagemCarro = new ImageIcon(image);
+            repaint();
+        }
+        
+    }
+    
+        // Métodos necessários da interface KeyListener
+        @Override
+        public void keyTyped(KeyEvent e) {
+            
+        }
+        
+        @Override
+        public void keyReleased(KeyEvent tecla) {
+            int codigo = tecla.getKeyCode();
+            if (codigo == KeyEvent.VK_LEFT || codigo == KeyEvent.VK_RIGHT) {
+                this.image = "./carro.png"; // Volta para a imagem padrão do carro
+                this.imagemCarro = new ImageIcon(image); // Atualiza a imagem do carro
+            }
+        }
 
+    
+    
 }
